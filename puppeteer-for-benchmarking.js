fd5067bfce53fb846ca887a03e17e3a0a1f3464b
Sanket Mehrotra <sanketmehrotra102@gmail.com>
2020-12-06 10:38:01 -0700

    reorganized code, moved all tracelyzer code into puppetteer tracealyzer and tried to revert puppeteer-for-benchmarking.js to it's older format. May need to check with older commits to get some code back. puppeteer-tracealyzer.js is not 50 lines of script.

--- puppeteer-for-benchmarking.js
+++ puppeteer-for-benchmarking.js
@@ -5,53 +5,66 @@ const fs = require('fs')
 const tracealyzer = require('tracealyzer');
 const { performance, PerformanceObserver} = require('perf_hooks')
 
-const tracepath = "./tracelyzer/";
-const windowperfpath = "./window.performance.timing/";
-const pagemetricspath = "./page.metrics/";
-const perfgetmetricspath = "./performance.getmetrics/";
-let filename = '1-state-metrics';
-// var stream1 = fs.createWriteStream(tracepath+filename, {flags:'a'});
-// var stream2 = fs.createWriteStream(windowperfpath+filename, {flags:'a'});
-// var stream3 = fs.createWriteStream(pagemetricspath+filename, {flags:'a'});
-// var stream4 = fs.createWriteStream(perfgetmetricspath+filename, {flags:'a'});
-
+var stream = fs.createWriteStream("./tracealyzer-metrics.txt", {flags:'a'});
+stream.write("This is the metrics file for the first four states census tract information. over 10 runs.\n");
 var i = 0;
 
 (async () => {
-        const browser = await puppeteer.launch();
-        for(i = 0; i < 10;i++) {
-            const context = await browser.createIncognitoBrowserContext();
-            const page = await context.newPage();
+    const browser = await puppeteer.launch();
+    for(i = 0; i < 10;i++) {
+        const context = await browser.createIncognitoBrowserContext();
+        const page = await context.newPage();
+
+        await page.tracing.start({path: './profile_' + i.toString() + '.json'});
+        await page.goto('http://localhost:63342/win_BD_experiment/clean-leaflet/', {waitUntil: 'load', timeout: 0});
 
 
-            await page.tracing.start({path: './profile_' + i.toString() + '.json'});
-            await page.goto('http://localhost:63342/win_BD_experiment/clean-leaflet/', {waitUntil: 'load', timeout: 0});
-            // await page.goto('http://urban-sustain.org/aperture3/aperture-client/', {waitUntil: 'load', timeout: 0});
-            // await page.waitFor(1000);
+        const performanceTiming = JSON.parse(
+            await page.evaluate(() => JSON.stringify(window.performance.timing))
+        );
+        // stream.write(JSON.stringify(performanceTiming));
+        // stream.write("\n\n\n")
+        // console.log(performanceTiming);
 
-            // //Code to dig into the iframe and select a fire station checkbox
-            // let myFrame;
-            // try{
-            //     for (const frame of page.mainFrame().childFrames()){
-            //         // Here you can use few identifying methods like url(),name(),title()
-            //         if (frame.url().includes('map2.html')){
-            //             console.log('we found the map2 iframe')
-            //             myFrame = frame
-            //             // we assign this frame to myFrame to use it later
-            //         }
-            //     }
-            //
-            // } catch (error){
-            //     console.log("iframe/element related error" + error.toString());
-            // }
+        // let
+        //     dns  = performanceTiming.domainLookupEnd - performanceTiming.domainLookupStart,
+        //     tcp  = performanceTiming.connectEnd - performanceTiming.connectStart,
+        //     ssl = performanceTiming.requestStart - performanceTiming.secureConnectionStart,
+        //     waitingTime = performanceTiming.responseStart - performanceTiming.requestStart,
+        //     contentTime = performanceTiming.responseEnd - performanceTiming.responseStart,
+        //     networkTime = (dns + tcp + waitingTime + contentTime),
+        //     pageloadTime = performanceTiming.loadEventStart - performanceTiming.navigationStart;
+        //
+        // console.log("\n\n");
+        // console.log("DNS Lookup time: " + dns.toString());
+        // console.log("TCP Handshake time: " + tcp.toString());
+        // console.log("SSL Handshake time: " + ssl.toString());
+        // console.log("Page Load time: " + pageloadTime.toString());
 
-            await page.tracing.stop();
-            await context.close();
-            const metrics = tracealyzer('./profile_' + i.toString() + '.json');
+        await page._client.send('Performance.enable');
+        const performanceMetrics = await page._client.send('Performance.getMetrics');
+        // const performanceMetrics = JSON.parse(
+        //     await page.evaluate(() => JSON.stringify(performance.get))
+        // );
+        // console.log("\n\n\n");
+        // console.log(performanceMetrics);
+        // performanceMetrics['metrics'].forEach((m) => {
+        //     stream.write(m.toString());
+        // })
+        // stream.write("\n\n\n")
 
-            filename = filename + '_run_' + i.toString() + '.json';
-            var stream1 = fs.createWriteStream(tracepath+filename, {flags:'a'});
-            stream1.write(JSON.stringify(metrics));
-        }
-        await browser.close();
-    })();
+        const perf = await page.metrics();
+        // console.log(JSON.parse(JSON.stringify(perf)));
+        // stream.write(JSON.stringify(perf));
+        // stream.write("\n\n\n\n\n")
+
+        await page.tracing.stop();
+        await context.close();
+
+        const metrics = tracealyzer('./profile_' + i.toString() + '.json');
+        console.log(metrics['profiling']['categories']);
+        stream.write(JSON.stringify(metrics['profiling']['categories']).toString());
+    }
+    await browser.close();
+
+})();
