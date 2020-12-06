1bc4e1a2f97e4005893b6e61f872b5fa3c61bc32
Sanket Mehrotra <sanketmehrotra102@gmail.com>
2020-12-06 13:48:27 -0700

    tried to use node find it to find and delete files - but haven't implemented in code yet. using both window.perf.timing and tracealyzer output for now.

--- puppeteer-tracealyzer.js
+++ puppeteer-tracealyzer.js
@@ -5,8 +5,20 @@ const fs = require('fs')
 const tracealyzer = require('tracealyzer');
 const { performance, PerformanceObserver} = require('perf_hooks')
 
-var stream = fs.createWriteStream("./tracealyzer-metrics.txt", {flags:'a'});
-stream.write("This is the metrics file for the first four states census tract information. over 10 runs.\n");
+let starttime = Date.now()
+console.log(starttime);
+
+
+const tracepath = "./tracelyzer/";
+const windowperfpath = "./window.performance.timing/";
+// const pagemetricspath = "./page.metrics/";
+// const perfgetmetricspath = "./performance.getmetrics/";
+const major_filename = '4-state-metrics';
+// var stream1 = fs.createWriteStream(tracepath+filename, {flags:'a'});
+var stream2 = fs.createWriteStream(windowperfpath+major_filename, {flags:'a'});
+// var stream3 = fs.createWriteStream(pagemetricspath+filename, {flags:'a'});
+// var stream4 = fs.createWriteStream(perfgetmetricspath+filename, {flags:'a'});
+
 var i = 0;
 
 (async () => {
@@ -15,56 +27,94 @@ var i = 0;
         const context = await browser.createIncognitoBrowserContext();
         const page = await context.newPage();
 
+
         await page.tracing.start({path: './profile_' + i.toString() + '.json'});
         await page.goto('http://localhost:63342/win_BD_experiment/clean-leaflet/', {waitUntil: 'load', timeout: 0});
+        // await page.goto('http://urban-sustain.org/aperture3/aperture-client/', {waitUntil: 'load', timeout: 0});
+        // await page.waitFor(1000);
 
-
+        // //Code to dig into the iframe and select a fire station checkbox
+        // let myFrame;
+        // try{
+        //     for (const frame of page.mainFrame().childFrames()){
+        //         // Here you can use few identifying methods like url(),name(),title()
+        //         if (frame.url().includes('map2.html')){
+        //             console.log('we found the map2 iframe')
+        //             myFrame = frame
+        //             // we assign this frame to myFrame to use it later
+        //         }
+        //     }
+        //
+        // } catch (error){
+        //     console.log("iframe/element related error" + error.toString());
+        // }
         const performanceTiming = JSON.parse(
             await page.evaluate(() => JSON.stringify(window.performance.timing))
         );
-        // stream.write(JSON.stringify(performanceTiming));
-        // stream.write("\n\n\n")
+        stream2.write(JSON.stringify(performanceTiming));
+        stream2.write("------")
         // console.log(performanceTiming);
-
-        // let
-        //     dns  = performanceTiming.domainLookupEnd - performanceTiming.domainLookupStart,
-        //     tcp  = performanceTiming.connectEnd - performanceTiming.connectStart,
-        //     ssl = performanceTiming.requestStart - performanceTiming.secureConnectionStart,
-        //     waitingTime = performanceTiming.responseStart - performanceTiming.requestStart,
-        //     contentTime = performanceTiming.responseEnd - performanceTiming.responseStart,
-        //     networkTime = (dns + tcp + waitingTime + contentTime),
-        //     pageloadTime = performanceTiming.loadEventStart - performanceTiming.navigationStart;
-        //
-        // console.log("\n\n");
-        // console.log("DNS Lookup time: " + dns.toString());
-        // console.log("TCP Handshake time: " + tcp.toString());
-        // console.log("SSL Handshake time: " + ssl.toString());
-        // console.log("Page Load time: " + pageloadTime.toString());
-
-        await page._client.send('Performance.enable');
-        const performanceMetrics = await page._client.send('Performance.getMetrics');
-        // const performanceMetrics = JSON.parse(
-        //     await page.evaluate(() => JSON.stringify(performance.get))
-        // );
-        // console.log("\n\n\n");
-        // console.log(performanceMetrics);
-        // performanceMetrics['metrics'].forEach((m) => {
-        //     stream.write(m.toString());
-        // })
-        // stream.write("\n\n\n")
-
-        const perf = await page.metrics();
-        // console.log(JSON.parse(JSON.stringify(perf)));
-        // stream.write(JSON.stringify(perf));
-        // stream.write("\n\n\n\n\n")
 
         await page.tracing.stop();
         await context.close();
-
         const metrics = tracealyzer('./profile_' + i.toString() + '.json');
-        console.log(metrics['profiling']['categories']);
-        stream.write(JSON.stringify(metrics['profiling']['categories']).toString());
+
+        const filename = major_filename + '_run_' + i.toString() + '.json';
+        var stream1 = fs.createWriteStream(tracepath+filename, {flags:'a'});
+        stream1.write(JSON.stringify(metrics));
+        stream1.close();
+        console.log(Date.now() - starttime)
     }
     await browser.close();
-
 })();
+
+
+//https://stackoverflow.com/questions/25460574/find-files-by-extension-html-under-a-folder-in-nodejs
+var path = require('path');
+
+function fromDir(startPath,filter,callback){
+
+    // console.log('Starting from dir '+startPath+'/');
+
+    if (!fs.existsSync(startPath)){
+        console.log("no dir ",startPath);
+        return;
+    }
+
+    var files=fs.readdirSync(startPath);
+    for(var i=0;i<files.length;i++){
+        var filename=path.join(startPath,files[i]);
+        var stat = fs.lstatSync(filename);
+        if (stat.isDirectory()){
+            fromDir(filename,filter,callback); //recurse
+        }
+        else if (filter.test(filename)) callback(filename);
+    };
+};
+
+
+
+fromDir('./',/profile\_/,function(filename){
+    console.log('-- found: ',filename);
+    fs.unlink(filename, (err) => {
+        if (err) {
+            console.error(err)
+            fs.unlinkSync(filename)
+            return
+        }//file removed
+    })
+});
+
+// fs.access(path, fs.F_OK, (err) => {
+//     if (err) {
+//         console.error(err)
+//         return
+//     }
+//     //file exists
+//     fs.unlink(path, (err) => {
+//         if (err) {
+//             console.error(err)
+//             return
+//         }//file removed
+//     })
+// })
